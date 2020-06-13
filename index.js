7edb908296c7078274fe362a8d1fe59261af8d1d
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-14 01:29:12 +0300

    mongoose again

--- index.js
+++ index.js
@@ -8,24 +8,24 @@
 
 // 3rd party lib
 require('dotenv').config();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // internal modules
 const server = require('./src/server.js');
 
-// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 
 
 server.start(PORT);
 
 
-// const mongooseOptions = {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// };
+const mongooseOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
 
-// mongoose.connect(MONGODB_URI, mongooseOptions);
+mongoose.connect(MONGODB_URI, mongooseOptions);
 
 
