dc574b8aadaa2ee0f306edc0bc0378afc88f7e59
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-14 19:42:23 +0300

    add image and displayName

--- src/lib/models/products/products.schema.js
+++ src/lib/models/products/products.schema.js
@@ -20,6 +20,7 @@ const products = mongoose.Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   inStock: { type: Number, required: true },
+  img: { type: String, required: false },
 });
 
 module.exports = mongoose.model('products', products);
